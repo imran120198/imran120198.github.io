659fed1c6704cf112a70609a525e7caa8614a41e
Imran Khan <99397606+imran120198@users.noreply.github.com>
2024-07-30 11:08:41 +0530

    update

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -11,6 +11,7 @@ import { SiExpress } from "react-icons/si";
 import { SiMongodb } from "react-icons/si";
 import Beautiholic from "../../resource/Beautiholic.png";
 import chatbot from "../../resource/chatbot.png";
+import translator from "../../resource/Translator.png"
 
 const Projects = () => {
   return (
@@ -48,7 +49,7 @@ const Projects = () => {
               </div>
               <div>
                 <a
-                  href="https://clinquant-semifreddo-8e54b8.netlify.app/"
+                  href="https://imaginative-baklava-6cdda4.netlify.app/"
                   target="_blank"
                   rel="noreferrer"
                 >
@@ -155,6 +156,52 @@ const Projects = () => {
                 </a>
                 <a
                   href="https://github.com/imran120198/Chatbot-flow-builder"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span type="button" className="btn btn--outline">
+                    View Code
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Language Translation App */}
+        <div className="projects_container">
+          <div className="project">
+            <div className="project_videocontainer">
+              <div data-aos="fade-right">
+                <img src={translator} alt="translator" />
+              </div>
+            </div>
+            <div className="project_information" data-aos="fade-right">
+              <h2>Translator</h2>
+              <p>
+                This project is about building a web application to translate
+                languages using language translator API from external services.
+                Here you will be able to translate between multiple languages
+                and whenever you type something it will be automatically
+                translated in the side panel.
+              </p>
+              <div>
+                <FaReact />
+                <IoLogoJavascript />
+                <DiCss3 />
+              </div>
+              <div>
+                <a
+                  href="https://eloquent-daifuku-05b55c.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span type="button" className="btn btn--outline onbt">
+                    See this Live
+                  </span>
+                </a>
+                <a
+                  href="https://github.com/imran120198/Translation-App"
                   target="_blank"
                   rel="noreferrer"
                 >
